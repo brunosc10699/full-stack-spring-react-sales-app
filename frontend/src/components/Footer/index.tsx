37b77f81fa2698723a195a9652553f75a352c324
brunosc10699 <brunosc10699@gmail.com>
2021-09-06 19:32:07 -0300

    basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Developed by 
+                    <a href="https://github.com/brunosc10699" target="_blank" rel="noreferrer">
+                        <span className="text-primary"> Leonardo</span>
+                    </a>
+                </p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                This event was promoted by DevSuperior school: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
