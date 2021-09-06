6965df60071fb438306f6d1ff0779521c5a754cb
brunosc10699 <brunosc10699@gmail.com>
2021-09-06 17:40:48 -0300

    project created

---
+++ backend/src/test/java/com/bruno/sds4/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.sds4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
